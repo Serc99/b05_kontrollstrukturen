fd5e7ee8fa122d6ae5f4f93f679e36b742863508
Sercan Yildiz <sercan.yz@outlook.de>
2025-10-07 02:30:59 -0700

    Create initial commit with the b_forSchleife sample.

---
+++ b_for/ForSchleife.java
@@ -0,0 +1,11 @@
+public class ForSchleife
+{
+   public static void main (String[] args)
+   {
+      for(int i = 1; i <= 20; i++)
+      {
+         System.out.println("Hallo " + i);
+      }
+   }
+}
+
