055d95a4af009330379e1175c6730df740b6f5da
Sercan Yildiz <sercan.yz@outlook.de>
2025-10-10 00:16:47 -0700

    Create new file for ABCAnalyse3 sample

---
+++ g_geschachteltes_if/ABCAnalyse3.java
@@ -0,0 +1,32 @@
+// b05_kontrollstruktur
+// g_geschachteltes_if
+
+// Schlechter Code
+
+public class ABCAnalyse3
+{
+   public static void main(String[] args)
+   {
+      char prioritaet = 'X';
+
+      if(prioritaet == 'A')
+      {
+         System.out.println("Hoch");
+      }
+
+         if(prioritaet == 'B')
+         {
+            System.out.println("Mittel");      
+         }
+         else
+         {
+            System.out.println("Niedrig");
+         }
+
+      if(prioritaet == 'X')
+      {
+         System.out.println("Unzulaessiger Wert");
+      }         
+   }   
+}
+
